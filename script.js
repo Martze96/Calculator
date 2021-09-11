5928e95909d5de524bb2a1ae0ef51fc1ea9d2b02
Martin Maul <martin.maul@hs-weingarten.de>
2021-09-11 11:22:48 +0200

    Done for now

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
+/******************************CONSTANTS*************************************/
 const ERROR_DIVIDE_BY_ZERO = 'Division by Zero does not work here.';
 const ERROR_NO_VALID_OPERATOR = 'Not a valid Operator.';
-
-
 
 const mainResult = document.getElementById('mainResult');
 const upperResult = document.getElementById('upperResult');
@@ -10,109 +9,119 @@ const operatorButtons = Array.from(document.getElementsByClassName('operator'));
 const clearButton = document.getElementById('clearButton');
 const deleteButton = document.getElementById('deleteButton');
 const resultButton = document.getElementById('resultButton');
+const commaButton = document.getElementById('commaButton');
+const infoBox = document.getElementById('infobox');
 
-let firstNum = null;
-let secondNum = null;
+/******************************GLOBALS***************************************/
 
-let inputArray = new Array();
-let input = 0;
-let contcalc = false;
-let resultHistory = 0;
+let first = '';
+let second = '';
+let currentOperator = '';
+let res = '';
+
+/******************************EVENTSLISTENERS*******************************/
 
 numberButtons.forEach(numberButton => {
     numberButton.addEventListener("click", () => {
-        //append or replace number? (replace if mainresult = 0)
-        input = Number(mainResult.textContent) === 0 ? 
-        input = Number(numberButton.textContent) : Number(input += numberButton.textContent); 
-        //set input to mainResult
-        mainResult.textContent = input;
+
+        if(first == '' && second == ''){
+                first = appendNumber(numberButton.textContent);
+                updateMainResultScreen(first);
+        }   else if (first != '' && second == '' && currentOperator == ''){
+                first = appendNumber(numberButton.textContent);
+                updateMainResultScreen(first);
+        }   
+            else if(first != '' && second == '' && currentOperator != ''){
+                second = appendNumber(numberButton.textContent);
+                updateMainResultScreen(second);
+        }
+            else {
+                second = appendNumber(numberButton.textContent);
+                updateMainResultScreen(second);
+        }
+
     })
 })
 
 operatorButtons.forEach(operatorButton => {
     operatorButton.addEventListener("click", () => {
-        inputArray.push(input);
-        //If yet nothing saved in input (but could be inputted without pushed)
-        if(inputArray.length == 0) {
-            //push to inputarray
-            inputArray.push(input);
-            //push the clicked operator
-            inputArray.push(operatorButton.getAttribute('id'));
-        // else if first pos in array already initialiazed, then only push the operator  
-        } else if(inputArray.length == 1) {
-            // only put the operator in inputArray
-            
-            inputArray.push(operatorButton.getAttribute('id'));
-        // else if number + operator + number and u press operator, it should calc the result, set the inputarray[0] to result and push operator    
-        } else if(inputArray.length == 3) {
-            console.log(inputArray);
-            let result = operate(inputArray[0],inputArray[1],inputArray[2])
-            inputArray = [];
-            inputArray.push(result);
-            inputArray.push(operatorButton.getAttribute('id'));
-            console.log(inputArray);
-        }
-        
-        //if operand clicked after operand, last operand changes
-        if(isNaN(inputArray[inputArray.length-1])){
-            inputArray[inputArray.length-1] = operatorButton.getAttribute('id');
+
+
+        if(first != '' && second == ''){
+            currentOperator = operatorButton.getAttribute('id');
+            updateUpperResultScreen(first + ' ' + currentOperator);
+            updateMainResultScreen(0);
+        }  
+
+        else if (second != '' && first != ''){
+            res = operate(first,currentOperator,second);
+            first = res;
+            currentOperator = operatorButton.getAttribute('id');
+            updateUpperResultScreen(first + ' ' + currentOperator);
+            updateMainResultScreen(0);
+            second = '';
         } 
-        //
-        upperResult.textContent = '';
-        inputArray.forEach(pos => {
-            upperResult.textContent += String(pos) + ' '; 
-        })
         
-        //upperResult.textContent = String(inputArray[0]) + ' ' + String(inputArray[1]);
-        input = 0;
-        mainResult.textContent = '0';
+        else {
+            currentOperator = operatorButton.getAttribute('id');
+            updateUpperResultScreen(first + ' ' + currentOperator);
+            return;
+        }
     })
 })
 
 clearButton.addEventListener("click", () => {
-    inputArray = [];
-    input = 0;
-    mainResult.textContent = input;
-    upperResult.textContent = '';
-    resultHistory = 0;
-    console.log(input);
+    updateMainResultScreen(0);
+    updateUpperResultScreen('');
+    first = '';
+    second = '';
+    currentOperator = '';
 })
 
 deleteButton.addEventListener("click", () => {
-    input = Math.floor(input / 10);
-    mainResult.textContent = input;
-    console.log(input);
+    if(first != ''){
+        if(first != '' && second == ''){
+            first = Math.floor(first/10);
+            mainResult.textContent = first;
+        } 
+        else {
+            second = Math.floor(second/10);
+            mainResult.textContent = second;
+        }
+    } else {
+        return;
+}
+
+
 })
 
 resultButton.addEventListener("click", () => {
-    inputArray.push(input);
-    console.log(inputArray);
-    let result = 0;
-    if(inputArray.length == 1) {
-        result = operate(inputArray[0],'+',0);
-        upperResult.textContent = result;
-    } else {
-        result = operate(inputArray[0],inputArray[1],inputArray[2]);
-        upperResult.textContent = String(inputArray[0]) + ' ' + String(inputArray[1]) + ' ' + String(inputArray[2] + ' ' + '=')
+    if(first == '' && second == ''){
+        return;
     }
-     
-    mainResult.textContent = result;
-    
-    input = result;
-    //resultHistory = Number(mainResult.textContent);
-    console.log(resultHistory);
-    inputArray = [result];
-    console.log(inputArray);
+    if(first != '' && second == '') {
+        mainResult.textContent = first;
+    }
+    if(first != '' && second != ''){
+        res = operate(first,currentOperator,second);
+        upperResult.textContent = first + ' ' + currentOperator + ' ' + second + ' = '
+        mainResult.textContent = res;
+        first = res;
+        second = '';
+    }
+
 })
 
+commaButton.addEventListener("click", () => {
+    alert("Point currently out of Office :)");
+});
+
+window.addEventListener('keydown', handleKeyboardInput);
 
 
+/******************************FUNCTIONS*******************************/
 
-/* Versuch onchange bei input ( evtl. einfach methoden machen)
-input.addEventListener("onchange", (e) => {
-    mainResult.textContent = e;
-})
-*/
+
 function operate(num1,operation,num2) {
 
     switch (operation) {
@@ -123,4 +132,73 @@ function operate(num1,operation,num2) {
         default: return ERROR_NO_VALID_OPERATOR;
     }
 }
+
+function appendNumber(input) {
+    // Cant input global variable, so same condition tree as eventlistener 
+    //to decide if to var first oder var second should be appended/replaced
+    let num = 0;
+    if          (first == '' && second == '')                          {num = first;}   
+        else if (first != '' && second == '' && currentOperator == '') {num = first;}   
+        else if (first != '' && second == '' && currentOperator != '') {num = second;}  
+        else                                                           {num = second;}
+    
+    if(input == 0 && num == ''){
+        alert('no leading zeros please.')
+        num = '';
+    }
+    
+        num = mainResult.textContent === 0 ? 
+    Number(input) : Number(String(num += input)); 
+    return num;
+    
+}
+
+function updateMainResultScreen(num) {
+    mainResult.textContent = num;
+}
+
+function updateUpperResultScreen(str) {
+    upperResult.textContent = str;
+}
+
+function handleKeyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9) document.getElementById(e.key).click();
+    if (e.key === '=' || e.key === 'Enter') document.getElementById('resultButton').click();
+    if (e.key === 'Backspace') document.getElementById('deleteButton').click();
+    if (e.key === 'Escape') document.getElementById('clearButton').click();
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+        document.getElementById(e.key).click();
+  }
+
+/****************************LAYOUT LISTENERS***********************************/
+
+window.addEventListener("resize", () => {
+    infoBox.style.visibility = window.innerWidth < 1153 ? 'hidden':'visible';
+})
+
+// Display Number Overflow Handler with Mutationobserver
+let observer = new MutationObserver(() => {
+    console.log(mainResult.clientWidth);
+    if(mainResult.clientWidth > document.getElementById('screen').clientWidth){
+        mainResult.textContent = '*OVERFLOW*';
+        upperResult.textContent = '';
+    }
+})
+
+let config = {characterData: false, subtree: true, childList: true};
+observer.observe(mainResult,config);
+
+
+
+
+/*
+=== Leading Zero does not work with this concept ( You'll get an alert :) )===
+BUG geteilt durch 0 macht keinen Fehler
+BUG 4*0 + 3 (es geht nicht mit 0 sondern mit 4 weiter, sollte aber 0 + im upper sein)
+================================================================================
+TODO footer hinzufügen (schauen wie das so geht mit github logo etc, oder ist das bei GitHub Pages normal?)
+*/
+
+
+
 
